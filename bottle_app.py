7b1bcd36b9dab0344e9ad25a9db90a9be27ab52b
Helga Lukaj <h.lukaj98@gmail.com>
2017-12-24 16:25:31 +0300

    Step3 done

--- bottle_app.py
+++ bottle_app.py
@@ -17,25 +17,30 @@ def htmlify(title,text):
             <head>
                 <meta charset="utf-8" />
                 <title>%s</title>
+             
+	
                 <style>
 			body{
-				background-color : #8FD3E7;
+				background-color : #DCD5D5;
 				text-align : center;
+				background-image:url("https://travel.jumia.com/blog/ng/wp-content/uploads/2016/08/airplane-wallpaper-2.jpg");
+				background-size: cover; 
+				position: relative;
+	
 			}
 			select{
 				width:30%%;
 				text-align:center;
 			}
 			table{
-				margin-left:15%%;
-				margin-right:15%%  ;
+				margin-left:25%%;
 				border : 1,5 px solid black;
 				width : 50%%;
 				border-collapse:collapse;
 			}
 			th{
-				height : 50px;
-				background-color : #4CAF50;
+				height : 30px;
+				background-color : #000000;
 				color : white;
 			}
 			td{
@@ -48,18 +53,43 @@ def htmlify(title,text):
 				background-color : #26EF8A;
 			}
 			.L{
-				background-color : #F74246;
+				background-color : #F95D5D;
 			}
 			a{
 				font-size:25px;
-				color:#878787;
+				color:#000000;
 			}
 			a:hover{
-				color:#961763;
+				color:#165070;
 			}
 			.submit{
-				width:10%%;
+				width:15%%;
 			}
+			p{
+			text-align : center;
+			margin-right: 80px;
+			margin-left: 80px;
+			font-family: 'BenchNine', sans-serif;
+			font-size: 0.5rem
+			text-shadow: white 90px 90px 90px;
+			}
+			h1  {
+			text-align:center;
+			font-weight: bold;
+			color: #4D4D4D;
+			font-size: 90px;
+			}
+			p{
+			text-align : center;
+			margin-right: 80px;
+			margin-left: 80px;
+			font-family: 'BenchNine', sans-serif;
+			font-size: 0.5rem
+			text-shadow: #FFFFFF 5px 5px 5px;
+			background-color:rgba(170,170,170,0.4);
+			
+			
+
 		</style>
             </head>
             <body>
@@ -72,6 +102,13 @@ def htmlify(title,text):
 
 def index():
 	text = """
+	<h1>Airline Safety</h1>
+	<h2>"Aviation safety costs little,but saves much."</h2>
+	<p>Safety of passengers in one of the most impotant thing we should consider when rating an airline.
+	People should choose their flying companies carefully 
+	by considering many factors,but the most important one should be being secure.This web app will 
+	help everyone to check over some data and think over their choice
+	</p>
 	<form action="/airlinename" method="POST">
 		<input type="textbox" name="airline" placeholder="Search By Airline Name"/>
 		<input type="submit" value="Search">
@@ -89,28 +126,18 @@ def index():
 		<input type="checkbox" name="column" value="3">Fatalities</input><br/>
 		<input type="submit" value="Search">
 	</form><br/>
-	<p>hffsgs8gs </p>
-	
+	<p>The dataset was collected over 54 airline companies.It involves number of km they fly in a week and 
+	number of incidents, fatalities and fatal accidents.This web app will help you compare the progress airlines
+	made from their beginnings 1985-1999 to these days 2000-2014.Check our web app to be sure if the company you are choosing is the
+	right one and if this company gives priority to its clients by  making their crafts more secure.If the number of incidents
+	is lower year by year this means that the airline invested to improve the quality.
+	</p>
+	<a href="https://github.com/ituis17/a3-helgalukaj">Assignment page</a>
 	"""
-	return htmlify("My lovely website",text)
+	return htmlify("Airline safety",text)
 
-def airlinename():
-	userinput = request.POST["airline"]
-	text ="""<table>
-			<tr>
-				<th>Airline Name</th>
-				<th>Kilometers Flown Every Week</th>
-				<th>Incidents, 1985–1999</th>
-				<th>Fatal Accidents, 1985–1999</th>
-				<th>Fatalities, 1985–1999</th>
-				<th>Incidents, 2000–2014</th>
-				<th>Fatal Accidents, 2000–2014</th>
-				<th>Fatalities, 2000–2014</th>
-			</tr>\n	
-				"""
-	for x in contents :
-		if userinput in x[0]:
-			text += """<tr>
+def addrow(x):
+	text = """<tr>
 					<td>%(airlinename)s</td>
 					<td>%(seat)s</td>
 					<td>%(i8599)s</td>
@@ -121,21 +148,45 @@ def airlinename():
 					<td>%(f0015)s</td>
 				</tr>
 			""" % {"seat" : x[1], "airlinename":x[0], "i8599":x[2], "a8599":x[3], "f8599":x[4], "i0015":x[5], "a0015":x[6] ,"f0015":x[7]}
+	return text
+def create_header(text):
+	text1 = """<table>
+			<tr>
+				%s
+			</tr>\n	
+				""" %text
+	return text1		
+
+def airlinename():
+	userinput = request.POST["airline"]
+	header = """<th>Airline Name</th>
+				<th>Kilometers Flown Every Week</th>
+				<th>Incidents, 1985–1999</th>
+				<th>Fatal Accidents, 1985–1999</th>
+				<th>Fatalities, 1985–1999</th>
+				<th>Incidents, 2000–2014</th>
+				<th>Fatal Accidents, 2000–2014</th>
+				<th>Fatalities, 2000–2014</th>"""
+	text = create_header(header)
+	for x in contents :
+		if x[0] == "AIRLINE":
+			continue
+		if userinput in x[0]:
+			text += addrow(x) 
 	text += "</table>"
 	return htmlify("Title", text)
+
+		
 
 def year():
 	userinput = request.POST["year"]
 	text = ""
 	if userinput == "1":
-		text ="""<table>
-			<tr>
-				<th>Airline Name</th>
+		header = """<th>Airline Name</th>
 				<th>Incidents, 1985–1999</th>
 				<th>Fatal Accidents, 1985–1999</th>
-				<th>Fatalities, 1985–1999</th>
-			</tr>\n	
-				"""
+				<th>Fatalities, 1985–1999</th>"""
+		text =create_header(header)
 		for x in contents :
 			if x[0] == "AIRLINE":
 				continue
@@ -147,14 +198,11 @@ def year():
 							</tr>
 			""" % {"airlinename":x[0], "i8599":x[2], "a8599":x[3], "f8599":x[4]}		
 	else :
-		text ="""<table>
-			<tr>
-				<th>Airline Name</th>
+		header = """<th>Airline Name</th>
 				<th>Incidents, 2000–2014</th>
 				<th>Fatal Accidents, 2000–2014</th>
-				<th>Fatalities, 2000–2014</th>
-			</tr>\n	
-				"""
+				<th>Fatalities, 2000–2014</th>"""
+		text =create_header(header)
 		for x in contents :
 			if x[0] == "AIRLINE":
 				continue
